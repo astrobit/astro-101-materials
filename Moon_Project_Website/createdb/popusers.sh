0eaa872d30481d53a9d4166d99899c9abe8a13f8
Brian W. Mulligan <bwmulligan@astronaos.com>
2020-07-22 11:51:14 -0500

    remove populate users; need too much information to do this here

--- Moon_Project_Website/createdb/popusers.sh
+++
@@ -1,6 +0,0 @@
-DBSERVER='tcp://127.0.0.1:3306'
-DBUSER='moonproject'
-DBPASS='7c3J0r*nO0M#0rD$s@P'
-DBDB='moon_project'
-
-../sqlremote/sqlremote -s $DBSERVER -u $DBUSER -p $DBPASS -d $DBDB -f populate_users.sql

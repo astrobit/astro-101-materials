691f2c47652f464ce0ac6173461f1714354c5200
Brian W. Mulligan <bwmulligan@astronaos.com>
2021-07-01 16:57:28 -0500

    add file for physical and astronomical constants

---
+++ common/commonConst.js
@@ -0,0 +1,12 @@
+const kPlanck = 6.62607015e-27; // erg s
+const kPlanck_eV = 4.135667696e-15; // eV s
+const kSpeedLight = 29979245800.0; // cm/s
+const kSpeedLight_kms = 299792.458000; // km/s
+const kAstronomicalUnit = 14959787070000.0; // cm
+const kParsec_au = 3600.0 * 180.0 / Math.PI; // au / pc
+const kParsec_cm = 3600.0 * 180.0 / Math.PI * kAstronomicalUnit; // au / pc
+const kLuminositySolar = 3.828e33; // erg/s
+const kMassSolar = 1.9885e33; // g
+const kRadiusSolar = 6.96342e10; // cm
+const kMVSolar = 4.83; // Absolute Magnitude (V) of Sun
+

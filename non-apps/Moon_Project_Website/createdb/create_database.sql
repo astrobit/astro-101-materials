3715543fb77b32923007071d64addda5b9dca147
Brian W. Mulligan <bwmulligan@astronaos.com>
2022-09-26 00:02:57 -0500

    remove non-web app materials from this repository

--- non-apps/Moon_Project_Website/createdb/create_database.sql
+++
@@ -1,7 +0,0 @@
-
-CREATE DATABASE moon_project;
-CREATE USER moonproject IDENTIFIED BY "7c3J0r*nO0M#0rD$s@P";
-GRANT ALL ON moon_project to moonproject;
-
-
-
--- non-apps/Moon_Project_Website/images/mooncv.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for image in moonFQ moonFull moonNew moonTQ moonWnC moonWnG moonWxC moonWxG
-do
-	convert ${image}.png -scale 16 ${image}16.png
-	convert ${image}.png -scale 24 ${image}24.png
-done

72d5af6293dfb0e956a115f45f5fa55b7446f022
Brian W. Mulligan <astrobit2@yahoo.com>
2023-03-29 18:56:31 -0500

    test getFileBinary

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@
 
 let result = null;
 
-let _dataPromise = getFile("https://www.astronaos.com/astronomy/images/real_data/chandra_114.fits");
+let _dataPromise = getFileBinary("https://www.astronaos.com/astronomy/images/real_data/chandra_114.fits");
 if (typeof _dataPromise != 'undefined' && _dataPromise !== null)
 	_dataPromise.then(function(value){result = value},function(error){console.log("request failed with error " + error)});
 

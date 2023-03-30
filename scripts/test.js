a4924ba32805c8bff0dc49d4a3088fdd1bce1039
Brian W. Mulligan <astrobit2@yahoo.com>
2023-03-29 19:04:31 -0500

    resolve blob data?

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,24 @@
 "use strict";
 
-let result = null;
+let blobResult = null;
+let blobArray = null;
 
 let _dataPromise = getFileBinary("https://www.astronaos.com/astronomy/images/real_data/chandra_114.fits");
 if (typeof _dataPromise != 'undefined' && _dataPromise !== null)
-	_dataPromise.then(function(value){result = value},function(error){console.log("request failed with error " + error)});
+	_dataPromise.then(function(value){blobResult = value},function(error){console.log("request failed with error " + error)});
 
 
 function waiter()
 {
-	if (result != null)
+	if (blobArray != null)
 	{
-		console.log("ready");
+		console.log("array ready");
+	}
+	else if (blobResult != null)
+	{
+		console.log("blob ready");
+		blobResult.arrayBuffer().then(function(value){blobArray = value},function(error){console.log("blobl arrray request failed with error " + error)});
+		window.setTimeout(waiter, 1000.0);
 	}
 	else
 	{

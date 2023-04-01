f21f08651e404c334c0db2db4c7e8985aa10a330
Brian W. Mulligan <astrobit2@yahoo.com>
2023-03-31 20:22:22 -0500

    display pixel info

--- scripts/testbed.js
+++ scripts/testbed.js
@@ -6,9 +6,39 @@ theCanvas.width = window.innerWidth;
 
 let theContext = theCanvas.getContext("2d");
 
-
-
 let g_testFits = requestFITS("https://www.astronaos.com/astronomy/images/real_data/chandra_114.fits");
+
+function setOutputText(id,value)
+{
+	let elem = document.getElementById(id);
+	if (elem !== null)
+		elem.value = value;
+}
+
+
+theCanvas.onmousemove = function(event)
+{
+	g_MouseX = event.offsetX;
+	g_MouseY = event.offsetY;
+	if (event.offsetX >= 0 && event.offsetX <= theCanvas.width &&
+		event.offsetY >= 0 && event.offsetY <= theCanvas.height &&
+		g_testFits.ready)
+	{
+		y = ;
+		const counts = g_testFits.counts(event.offsetX,g_testFits.height - event.offsetY);
+		const radec = g_testFits.radec(event.offsetX,g_testFits.height - event.offsetY);
+		
+		const RAdispl = degreestoHMSDisplayable(radec.ra / 15.0);
+		const Decdispl = degreestoDMSDisplayable(radec.dec);
+
+		setOutputText("ra",RAdispl.hr + "h " + RAdispl.min + "m " + RAdispl.sec + "s");
+		setOutputText("dec",Decdispl.deg + "° " + Decdispl.min + "' " + Decdispl.sec + "\"");
+		setOutputText("x",radec.x);
+		setOutputText("y",radec.y);
+		setOutputText("counts",counts);
+		
+	}
+}
 
 function waiter()
 {

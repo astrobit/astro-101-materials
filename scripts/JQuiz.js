4695670639f7de7b73365d3b51b0d664f7f4fab2
Brian W. Mulligan <astrobit2@yahoo.com>
2023-04-11 22:10:20 -0500

    ast 1403 exam 3

--- scripts/JQuiz.js
+++ scripts/JQuiz.js
@@ -63,53 +63,113 @@ let currquestion = null;
 //}
 
 
+//let set = {
+//	title: "ASTR 1403 Exam 2 Review",
+//	categories: ["Blackbodies", "Spectra", "Sun", "Gravity", "Telescopes", "Potpurri"],
+//	questions: 
+//	[
+//		[
+//			[
+//				{q:"The law relates the temperature and wavelength at which a blackbody gives off most of its light.",a:"Wein's Law",complete:false},
+//				{q:"Blackbody luminosity is affected by temperature to this power",a:"Four (4)",complete:false},
+//				{q:"A 10000 K star gives off most of it's light as this.",a:"Ultraviolet (UV)",complete:false},
+//				{q:"One object has a temperature twice as large as another; it will be this many times brighter.",a:"16",complete:false},
+//				{q:"At object with a temperature of 500 K will give off mostly this form of light.",a:"Infrared",complete:false}
+//			],
+//			[
+//				{q:"An object moving away from you is said to have this.",a:"Redshift",complete:false},
+//				{q:"The spectrum that results from a blackbody.",a:"Continuum",complete:false},
+//				{q:"The spectrum that results from a cool gas in front of a blackbody",a:"Absorption Spectrum",complete:false},
+//				{q:"Emission lines from a gas are due to electrons jumping between these.",a:"Orbitals",complete:false},
+//				{q:"The spectrum that results from a hot gas, or from a cool gas near a blackbody observed from the side.",a:"Emission Spectrum",complete:false}
+//			],
+//			[
+//				{q:"These have a temperature of about 3000 K, much cooler than the rest of the surface",a:"Sunspots",complete:false},
+//				{q:"This internal region of the Sun carries heat from the radiative zone to the surface",a:"Convective Zone",complete:false},
+//				{q:"This tenuous gas is extremely hot and visible only during a total solar eclispe.",a:"Corona",complete:false},
+//				{q:"The 'surface' of the Sun; it gives off a continuum spectrum.",a:"Photosphere",complete:false},
+//				{q:"The region above the visible surface of the Sun responsible for the dark band in the Sun's spectrum.",a:"Chromosphere",complete:false}
+//			],
+//			[
+//				{q:"This law explains why things fall, and why planets orbit the Sun.", a: "Newton's law of gravity",complete:false},
+//				{q:"This law relates the period and semi-major axis of a planet orbiting the Sun.",a:"Kepler's 3rd Law",complete:false},
+//				{q:"The factor by which distance affects the force of gravity.",a:"-2 (1/ distance squared)",complete:false},
+//				{q:"By observing both the period and semi-major axis of a moon orbiting a planet, this can be found.",a:"Mass",complete:false},
+//				{q:"This property of matter determines it's inertia and gravitational influence .",a:"Mass",complete:false}
+//			],
+//			[
+//				{q:"The most common professional telescope that uses mirrors to focus the light.", a: "Reflecting",complete:false},
+//				{q:"The size of an Airy disk, or the limit of detail a telescope can observe.",a:"Diffraction Limit / Angular Resolution",complete:false},
+//				{q:"This property determines both the light gathering power and angular resolution of a single mirror telescope.",a:"Diameter",complete:false},
+//				{q:"The type of digital camera used to turn light into an electrical signal.",a:"Charge Coupled Device (CCD)",complete:false},
+//				{q:"The term for a system that reverses the effect of turbulence in the atmosphere",a:"Adaptive Optics",complete:false}
+//			],
+//			[
+//				{q:"The terrm for when light is shifted to a shorter wavelength due to motion of the source.", a: "Blueshift",complete:false},
+//				{q:"A device used to break up light into its wavelengths, used on a telescope.",a:"Spectrograph",complete:false},
+//				{q:"The term used to describe a shift in the wavelength in light due to motion",a:"Doppler Shift",complete:false},
+//				{q:"The term for a atom that has lost one or more electrons.",a:"Ion",complete:false},
+//				{q:"The term used to describe the splitting of electron orbitals due to a magnetic field",a:"Zeeman Effect",complete:false}
+//			]
+//		]
+//	]
+//}
+
+
 let set = {
-	title: "ASTR 1403 Exam 2 Review",
-	categories: ["Blackbodies", "Spectra", "Sun", "Gravity", "Telescopes", "Potpurri"],
+	title: "ASTR 1403 Exam 3 Review",
+	categories: ["Fusion", "Stars", "Star Formation", "Stellar Evolution", "White Dwarfs", "Super- novae", "Neutron Stars and Black Holes"],
 	questions: 
 	[
 		[
 			[
-				{q:"The law relates the temperature and wavelength at which a blackbody gives off most of its light.",a:"Wein's Law",complete:false},
-				{q:"Blackbody luminosity is affected by temperature to this power",a:"Four (4)",complete:false},
-				{q:"A 10000 K star gives off most of it's light as this.",a:"Ultraviolet (UV)",complete:false},
-				{q:"One object has a temperature twice as large as another; it will be this many times brighter.",a:"16",complete:false},
-				{q:"At object with a temperature of 500 K will give off mostly this form of light.",a:"Infrared",complete:false}
+				{q:"The element beyond which fusion can't release energy.",a:"Iron (Fe)",complete:false},
+				{q:"The end product of the p-p chain.",a:"Helium (He)",complete:false},
+				{q:"The temperature needed to star hydrogen fusion.",a:"10 - 15 Million K ",complete:false},
+				{q:"Carbon, nitrogen, or oxygen catalyze this form of fusion, which forms helium from hydrogen.",a:"CNO Cycle",complete:false},
+				{q:"This subatomic particle is released when a proton transforms into a neutron, or vice-versa.",a:"Neutrino",complete:false}
 			],
 			[
-				{q:"An object moving away from you is said to have this.",a:"Redshift",complete:false},
-				{q:"The spectrum that results from a blackbody.",a:"Continuum",complete:false},
-				{q:"The spectrum that results from a cool gas in front of a blackbody",a:"Absorption Spectrum",complete:false},
-				{q:"Emission lines from a gas are due to electrons jumping between these.",a:"Orbitals",complete:false},
-				{q:"The spectrum that results from a hot gas, or from a cool gas near a blackbody observed from the side.",a:"Emission Spectrum",complete:false}
+				{q:"The majority of stars can be found in this region of the H-R diagram or CMD.",a:"Main Sequence",complete:false},
+				{q:"This spectral type is hottest.",a:"O",complete:false},
+				{q:"This spectral type is coolest, and has the strongest molecular lines.",a:"M",complete:false},
+				{q:"The Sun is of this spectral type.",a:"G",complete:false},
+				{q:"The luminosity of a star is proportional to its mass to this power.",a:"Four (4)",complete:false}
 			],
 			[
-				{q:"These have a temperature of about 3000 K, much cooler than the rest of the surface",a:"Sunspots",complete:false},
-				{q:"This internal region of the Sun carries heat from the radiative zone to the surface",a:"Convective Zone",complete:false},
-				{q:"This tenuous gas is extremely hot and visible only during a total solar eclispe.",a:"Corona",complete:false},
-				{q:"The 'surface' of the Sun; it gives off a continuum spectrum.",a:"Photosphere",complete:false},
-				{q:"The region above the visible surface of the Sun responsible for the dark band in the Sun's spectrum.",a:"Chromosphere",complete:false}
+				{q:"This is the cause for a cloud to flatten into a disk, and for increasing rotation as a cloud shrinks",a:"Conservation of Angular Momentum",complete:false},
+				{q:"Blobs of gas that don't have enough material to become a proper star will become this.",a:"Brown Dwarf",complete:false},
+				{q:"This is the term for when a star first beomes a star; or more specifically, where it is on an H-R diagram at this moment.",a:"Zero Age Main Sequence (ZAMS)",complete:false},
+				{q:"The name for a dense core of gas that is opaque, but not yet a star.",a:"Protostar",complete:false},
+				{q:"Protorstars shoot out these to help them shrink further.",a:"Polar Jet",complete:false}
 			],
 			[
-				{q:"This law explains why things fall, and why planets orbit the Sun.", a: "Newton's law of gravity",complete:false},
-				{q:"This law relates the period and semi-major axis of a planet orbiting the Sun.",a:"Kepler's 3rd Law",complete:false},
-				{q:"The factor by which distance affects the force of gravity.",a:"-2 (1/ distance squared)",complete:false},
-				{q:"By observing both the period and semi-major axis of a moon orbiting a planet, this can be found.",a:"Mass",complete:false},
-				{q:"This property of matter determines it's inertia and gravitational influence .",a:"Mass",complete:false}
+				{q:"In this phase of a star's life, its used all of the hydrogen in the core. The star is cooling and expanding, while the core is shrinking and heating up.", a: "Red Giant",complete:false},
+				{q:"The phase of many stars life when it is fusing helium into carbon and oxygen in the core, and the star is mostly stable.",a:"Horizontal Branch",complete:false},
+				{q:"The penultimate stage of an intermediate mass star's life, when its core is exhausted of helium.",a:"Asymptotic Giant Branch",complete:false},
+				{q:"A term for when a star is fusing both hydrogen and helium outside of the core.",a:"Double Shell Burning",complete:false},
+				{q:"A term for the internal structure of a a massive star, which developed sucessively more massive elements.",a:"Onion Layers",complete:false}
 			],
 			[
-				{q:"The most common professional telescope that uses mirrors to focus the light.", a: "Reflecting",complete:false},
-				{q:"The size of an Airy disk, or the limit of detail a telescope can observe.",a:"Diffraction Limit / Angular Resolution",complete:false},
-				{q:"This property determines both the light gathering power and angular resolution of a single mirror telescope.",a:"Diameter",complete:false},
-				{q:"The type of digital camera used to turn light into an electrical signal.",a:"Charge Coupled Device (CCD)",complete:false},
-				{q:"The term for a system that reverses the effect of turbulence in the atmosphere",a:"Adaptive Optics",complete:false}
+				{q:"The maximum mass of a white dwarf, also known as the Chandrasekhar limit.", a: "1.44 Solar Masses",complete:false},
+				{q:"The type of pressure that balances gravity in a white dwarf.",a:"Degenerate Electron Pressure / Quantum Electron Pressure",complete:false},
+				{q:"Most white dwarfs are made of these two elements.",a:"Carbon and Oxygen",complete:false},
+				{q:"Most white dwarfs that are in a binary star system will become these bright events.",a:"Nova",complete:false},
+				{q:"Many stars create this large color cloud of gas and dust as they transition to becoming a white dwarf.",a:"Planetary Nebula",complete:false}
 			],
 			[
-				{q:"The terrm for when light is shifted to a shorter wavelength due to motion of the source.", a: "Blueshift",complete:false},
-				{q:"A device used to break up light into its wavelengths, used on a telescope.",a:"Spectrograph",complete:false},
-				{q:"The term used to describe a shift in the wavelength in light due to motion",a:"Doppler Shift",complete:false},
-				{q:"The term for a atom that has lost one or more electrons.",a:"Ion",complete:false},
-				{q:"The term used to describe the splitting of electron orbitals due to a magnetic field",a:"Zeeman Effect",complete:false}
+				{q:"This type of supernova is the brightest. It lacks hydrogen and helium in its spectra, and exhibits strong silicon absorption. They are explained as exploding white dwarfs.",a:"Type Ia",complete:false},
+				{q:"SN 1987A was one of this type of supernova, the result of a massive star running out of fuel.",a:"Type II",complete:false},
+				{q:"Type II supernova are expected to leave behind this remnant.", a:"Neutron Star",complete:false},
+				{q:"The result of some of the most massive stars exhausting their fuel. These supernova lack hydrogen, but may or may not have helium absorption lines.", a:"Type Ib/Ic Supernova",complete:false},
+				{q:"The approximate mass required for a main sequence star to go supernova.", a:"8 - 10 Solar Masses",complete:false}
+			],
+			[
+				{q:"The type of pressure that supports a neutron star.", a:"Neutron Degeneracy Pressure / Quantum Neutron Pressure",complete:false},
+				{q:"The name for a neutron star whose magnetic pole periodically points toward Earth.", a:"Pulsar",complete:false},
+				{q:"A term for the size of a black hole.", a: "Schwarzchild Radius / Event Horizon",complete:false},
+				{q:"This event occurs when two neutron stars merge together.", a: "Kilonova",complete:false},
+				{q:"Gamma ray bursts are thought to be the result of this.", a:"Black Hole Formation",complete:false}
 			]
 		]
 	]
@@ -163,7 +223,7 @@ function initialize()
 	let i;
 	questionholder.setAttribute("hidden","true");
 	title.innerHTML = set.title;
-	for (i = 1; i < 7; i++)
+	for (i = 1; i < 8; i++)
 	{
 		let cathdr = document.getElementById("cat" + i.toFixed(0));
 		cathdr.innerHTML = set.categories[i - 1];
